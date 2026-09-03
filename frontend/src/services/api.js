e04190506d48341df4687895818fd362eb60aba3
Sneha Jha <jhasneha988350@gmail.com>
2026-09-03 21:13:57 +0530

    Fix deployed API connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://inner-eye-employee-attendance-management.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
